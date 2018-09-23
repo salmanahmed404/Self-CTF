b0354973c23e72bb827f6f5a977fb780b03d839c
salmanahmed404 <36567032+salmanahmed404@users.noreply.github.com>
2018-09-23 19:10:36 +0530

    Delete fHeader.js

--- fHeader.js
+++
@@ -1,9 +0,0 @@
-var http = require('http');
-
-http.createServer(function (req,res){
-    //res.write("You might need md5");
-    res.setHeader('flag','bae2881227d46a00b6b5308c02a9e8d6');
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('ok');
-}).listen(7000);
-console.log("Listening");
